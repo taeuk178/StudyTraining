360d8556cf09779ea6ebb6bff08fe40860fca988
taeuk178 <nous178@naver.com>
2022-02-04 18:27:36 +0900

    Initial Commit

---
+++ StudyTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyTraining
+//
+//  Created by tw on 2022/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
